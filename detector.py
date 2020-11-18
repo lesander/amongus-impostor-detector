e176aa939e8968ef2538f58ce94342844a19f29a
lesander <lesander@users.noreply.github.com>
2020-11-18 18:44:12 +0100

    simplify display_filter

--- detector.py
+++ detector.py
@@ -1,7 +1,7 @@
 """
     Detect the impostor at game start
     https://github.com/lesander/amongus-impostor-detector
-    
+
     Requires Wireshark/tshark amongus protocol to be installed:
     https://github.com/cybershard/wireshark-amongus
 """
@@ -10,34 +10,20 @@ import pyshark
 
 class Detector:
 
-    def __init__(self, debug):
+    def __init__(self, debug=False):
         self.debug = debug
         self.impostors = []
         self.max_impostors = 4
-        self.udp_ports = [
-            22023, 22123, # 22223,
-            22323, 22423, 22523, 22623, 22723, 22823, 22923
-        ]
-
-    def _init_live_capture(self, interface):
-        udp_bpf = 'udp port 22023 or udp port 22723'
-        capture = pyshark.LiveCapture(interface=interface, bpf_filter=udp_bpf)
-        return capture
-
-    def _init_file_capture(self, path):
-        capture = pyshark.FileCapture(path, display_filter='udp.port == 22023 || udp.port == 22723')
-        return capture
 
     def start_live_capture(self, interface):
-        capture = self._init_live_capture(interface)
-        # capture.apply_on_packets(self.handle_packet)
+        capture = pyshark.LiveCapture(interface=interface, display_filter='amongus')
         for packet in capture.sniff_continuously():
             if self.handle_packet(packet):
                 capture.close()
                 break
 
     def start_file_capture(self, filepath):
-        capture = self._init_file_capture(filepath)
+        capture = pyshark.FileCapture(filepath, display_filter='amongus')
         for packet in capture:
             if self.handle_packet(packet):
                 capture.close()
@@ -63,8 +49,8 @@ class Detector:
                         self.impostors.append(impostor)
                 field_count += 1
 
-        if len(self.impostors) >= self.max_impostors:
-            return True
+            if len(self.impostors) >= self.max_impostors:
+                return True
 
     def get_impostors(self):
         return self.impostors
